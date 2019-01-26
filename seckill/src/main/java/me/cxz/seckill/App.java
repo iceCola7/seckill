8245e2c12e3eefd71d0cde79ae2aca2873c72253
chenxz <563859095@qq.com>
2019-01-26 12:55:39 +0800

    Initial commit

---
+++ seckill/src/main/java/me/cxz/seckill/App.java
@@ -0,0 +1,13 @@
+package me.cxz.seckill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
