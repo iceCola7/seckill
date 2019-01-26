4b03c31f9c370805f913d01619362c30f051a40f
chenxz <563859095@qq.com>
2019-01-26 13:44:26 +0800

    commit

--- seckill/src/main/java/me/cxz/seckill/App.java
+++ seckill/src/main/java/me/cxz/seckill/App.java
@@ -1,13 +1,43 @@
 package me.cxz.seckill;
 
+import me.cxz.seckill.dao.UserDOMapper;
+import me.cxz.seckill.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication(scanBasePackages = {"me.cxz.seckill"})
+@RestController
+@MapperScan("me.cxz.seckill.dao")
+public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/")
+    public String home() {
+
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "用户对象不存在";
+        } else {
+            return userDO.getName();
+        }
+
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        SpringApplication.run(App.class, args);
+
+    }
+
 }
