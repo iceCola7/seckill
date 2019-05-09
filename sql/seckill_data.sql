16fe3b03ca13bd3d551ef91ddad1aae30cb80702
chenxz <563859095@qq.com>
2019-05-09 23:06:41 +0800

    update sql

--- sql/seckill_data.sql
+++ sql/seckill_data.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50703
 File Encoding         : 65001
 
-Date: 2019-05-09 23:05:22
+Date: 2019-05-09 23:05:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,6 +30,12 @@ CREATE TABLE `item` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of item
+-- ----------------------------
+INSERT INTO `item` VALUES ('1', 'iphone6', '2499', '好用的苹果手机！！！', '122', 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1912792289,1459185699&fm=27&gp=0.jpg');
+INSERT INTO `item` VALUES ('2', 'iphone8', '4899', '最好用的苹果手机！！！', '221', 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3740963951,2121487327&fm=27&gp=0.jpg');
+
+-- ----------------------------
 -- Table structure for item_stock
 -- ----------------------------
 DROP TABLE IF EXISTS `item_stock`;
@@ -39,6 +45,12 @@ CREATE TABLE `item_stock` (
   `item_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of item_stock
+-- ----------------------------
+INSERT INTO `item_stock` VALUES ('1', '995', '1');
+INSERT INTO `item_stock` VALUES ('2', '1999', '2');
 
 -- ----------------------------
 -- Table structure for order_info
@@ -57,6 +69,16 @@ CREATE TABLE `order_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of order_info
+-- ----------------------------
+INSERT INTO `order_info` VALUES ('2019012500000000', '3', '1', '0', '0', '0', '0', '2019-05-01 23:01:52');
+INSERT INTO `order_info` VALUES ('2019012500000100', '3', '1', '2499', '0', '2499', '0', '2019-04-03 01:01:58');
+INSERT INTO `order_info` VALUES ('2019012500000200', '3', '1', '2499', '1', '2499', '0', '2019-05-03 12:02:11');
+INSERT INTO `order_info` VALUES ('2019012500000300', '3', '1', '2499', '1', '2499', '0', '2019-03-28 11:11:01');
+INSERT INTO `order_info` VALUES ('2019012500000400', '3', '1', '1000', '1', '1000', '1', '2019-05-03 11:32:33');
+INSERT INTO `order_info` VALUES ('2019050900000500', '1', '2', '4899', '1', '4899', '0', '2019-05-09 23:01:45');
+
+-- ----------------------------
 -- Table structure for promo
 -- ----------------------------
 DROP TABLE IF EXISTS `promo`;
@@ -71,6 +93,11 @@ CREATE TABLE `promo` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of promo
+-- ----------------------------
+INSERT INTO `promo` VALUES ('1', 'iphone6抢购活动', '2019-01-25 23:18:00', '1', '1000', '2019-05-22 22:30:00');
+
+-- ----------------------------
 -- Table structure for sequence_info
 -- ----------------------------
 DROP TABLE IF EXISTS `sequence_info`;
@@ -80,6 +107,11 @@ CREATE TABLE `sequence_info` (
   `step` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of sequence_info
+-- ----------------------------
+INSERT INTO `sequence_info` VALUES ('order_info', '6', '1');
 
 -- ----------------------------
 -- Table structure for user_info
@@ -98,6 +130,14 @@ CREATE TABLE `user_info` (
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES ('1', '张三', '0', '25', '15200001111', 'byphone', '', '2019-01-28 22:19:40');
+INSERT INTO `user_info` VALUES ('3', '哈哈', '1', '25', '15211112222', 'byphone', '', '2019-01-28 22:19:45');
+INSERT INTO `user_info` VALUES ('4', '第一个用户', '1', '26', '13200001111', 'byphone', '', '2019-01-29 22:19:50');
+INSERT INTO `user_info` VALUES ('12', '11', '1', '23', '18700001111', 'byphone', '', '2019-01-29 23:05:29');
+
+-- ----------------------------
 -- Table structure for user_password
 -- ----------------------------
 DROP TABLE IF EXISTS `user_password`;
@@ -107,3 +147,11 @@ CREATE TABLE `user_password` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of user_password
+-- ----------------------------
+INSERT INTO `user_password` VALUES ('1', 'ICy5YqxZB1uWSwcVLSNLcA==', '1');
+INSERT INTO `user_password` VALUES ('2', 'ICy5YqxZB1uWSwcVLSNLcA==', '3');
+INSERT INTO `user_password` VALUES ('3', 'ICy5YqxZB1uWSwcVLSNLcA==', '4');
+INSERT INTO `user_password` VALUES ('5', 'ICy5YqxZB1uWSwcVLSNLcA==', '12');
